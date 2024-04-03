28bcc9bfaf3ccb1624a390ec153fd674818cba61
Davi Borges <daviborges2014@gmail.com>
2024-04-02 22:59:40 -0300

    new colors

--- higieneshop-script.js
+++ higieneshop-script.js
@@ -78,3 +78,20 @@
     function finalizarCompra() {
         alert("Compra finalizada!");
     }
+
+    function buscarEndereco() {
+            var cep = document.getElementById('cepInput').value;
+            var url = `https://viacep.com.br/ws/${cep}/json/`;
+            
+            fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if(data.erro) {
+                    document.getElementById('endereco').innerText = "CEP não encontrado.";
+                } else {
+                    var endereco = `CEP: ${data.cep}, ${data.logradouro}, ${data.bairro}, ${data.localidade}, ${data.uf}`;
+                    document.getElementById('endereco').innerText = endereco;
+                }
+            })
+            .catch(error => console.error('Erro:', error));
+        }
